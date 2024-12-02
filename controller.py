7cbb363a5848e685b3bd8b7d8b2d0fa3dfc97285
antonilenart <antek@lenart.org.pl>
2024-12-02 14:36:38 +0100

    added icmp, tcp and udp match

---
+++ controller.py
@@ -0,0 +1,172 @@
+# Copyright (C) 2016 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.lib.packet.tcp import tcp
+from ryu.lib.packet.udp import udp
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib import dpid as dpid_lib
+from ryu.lib import stplib
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.app import simple_switch_13
+from ryu.lib.packet import ipv4
+from ryu import *
+
+
+class SimpleSwitch13(simple_switch_13.SimpleSwitch13):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    _CONTEXTS = {'stplib': stplib.Stp}
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.stp = kwargs['stplib']
+        # Sample of stplib config.
+        #  please refer to stplib.Stp.set_config() for details.
+        # config = {dpid_lib.str_to_dpid('0000000000000001'):
+        #           {'bridge': {'priority': 0x8000}},
+        #           dpid_lib.str_to_dpid('0000000000000002'):
+        #           {'bridge': {'priority': 0x9000}},
+        #           dpid_lib.str_to_dpid('0000000000000003'):
+        #           {'bridge': {'priority': 0xa000}}}
+        # self.stp.set_config(config)
+
+    def delete_flow(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        for dst in self.mac_to_port[datapath.id].keys():
+            match = parser.OFPMatch(eth_dst=dst)
+            mod = parser.OFPFlowMod(
+                datapath, command=ofproto.OFPFC_DELETE,
+                out_port=ofproto.OFPP_ANY, out_group=ofproto.OFPG_ANY,
+                priority=1, match=match)
+            datapath.send_msg(mod)
+
+    @set_ev_cls(stplib.EventPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        print(pkt)
+
+        # Parse IP packets and create a match using 5-tuple
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        if ip_pkt:
+            protocol = ip_pkt.proto
+            print(protocol)
+            print(ip_pkt)
+            match = parser.OFPMatch(
+                eth_type=0x0800,
+                ipv4_src=ip_pkt.src,
+                ipv4_dst=ip_pkt.dst,
+                ip_proto=protocol,
+            )
+
+            if protocol == 1:  # ICMP
+                match = parser.OFPMatch(
+                    eth_type=0x0800,
+                    ipv4_src=ip_pkt.src,
+                    ipv4_dst=ip_pkt.dst,
+                    ip_proto=protocol,
+                )
+
+            elif protocol == 6:  # TCP
+                tcp_pkt = pkt.get_protocol(tcp)
+                match = parser.OFPMatch(
+                    eth_type=0x0800,
+                    ipv4_src=ip_pkt.src,
+                    ipv4_dst=ip_pkt.dst,
+                    ip_proto=protocol,
+                    tcp_src=tcp_pkt.src_port,
+                    tcp_dst=tcp_pkt.dst_port,
+                )
+
+            elif protocol == 17:  # UDP
+                udp_pkt = pkt.get_protocol(udp)
+                match = parser.OFPMatch(
+                    eth_type=0x0800,
+                    ipv4_src=ip_pkt.src,
+                    ipv4_dst=ip_pkt.dst,
+                    ip_proto=protocol,
+                    udp_src=udp_pkt.src_port,
+                    udp_dst=udp_pkt.dst_port,
+                )
+
+            # Install a flow to avoid packet_in next time
+            self.add_flow(datapath=datapath, priority=1, match=match, actions=actions)
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+    @set_ev_cls(stplib.EventTopologyChange, MAIN_DISPATCHER)
+    def _topology_change_handler(self, ev):
+        dp = ev.dp
+        dpid_str = dpid_lib.dpid_to_str(dp.id)
+        msg = 'Receive topology change event. Flush MAC table.'
+        self.logger.debug("[dpid=%s] %s", dpid_str, msg)
+
+        if dp.id in self.mac_to_port:
+            self.delete_flow(dp)
+            del self.mac_to_port[dp.id]
+
+    @set_ev_cls(stplib.EventPortStateChange, MAIN_DISPATCHER)
+    def _port_state_change_handler(self, ev):
+        dpid_str = dpid_lib.dpid_to_str(ev.dp.id)
+        of_state = {stplib.PORT_STATE_DISABLE: 'DISABLE',
+                    stplib.PORT_STATE_BLOCK: 'BLOCK',
+                    stplib.PORT_STATE_LISTEN: 'LISTEN',
+                    stplib.PORT_STATE_LEARN: 'LEARN',
+                    stplib.PORT_STATE_FORWARD: 'FORWARD'}
+        self.logger.debug("[dpid=%s][port=%d] state=%s",
+                          dpid_str, ev.port_no, of_state[ev.port_state])
